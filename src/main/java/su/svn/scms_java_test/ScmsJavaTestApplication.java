3739b3ec10c5054c3826b965be83a29f4aed5cdc
Victor N. Skurikhin <VSkurikhin@GMail.Com>
2026-02-27 12:41:43 +0300

    First commit

---
+++ src/main/java/su/svn/scms_java_test/ScmsJavaTestApplication.java
@@ -0,0 +1,13 @@
+package su.svn.scms_java_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmsJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmsJavaTestApplication.class, args);
+	}
+
+}
